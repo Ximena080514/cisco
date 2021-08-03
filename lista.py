6ba4548be95ff0f89adc0b10264a929e538aed60
Ximena080514 <88395804+Ximena080514@users.noreply.github.com>
2021-08-03 11:41:54 -0500

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,8 @@
+lista =["hola", 7, 3.5 , True , 9 , "mundo", False , 4.5 ]
+print(lista)
+print(lista[4:7])
+lista.append ("curso")
+print (lista)
+lista [3]= lista [7]
+print (lista)
+print (lista[0:9:3])
